25219c5466b9412b730fcb7a15da2569aec204ac
Taher ElShoker <taher.elshoker@cartologic.com>
2020-09-01 16:56:33 +0200

    Add arabic text for (Other Development  Indicator) option in type indicators field

--- app/assets/scripts/components/widgets/ThemeField.js
+++ app/assets/scripts/components/widgets/ThemeField.js
@@ -4,7 +4,7 @@ import {sdsLabels, sdgLabels, otherDevLabels} from '../../utils/labels';
 const typeDisplayMap = {
   'sds': 'SDS Goal - هدف استراتيجية التنمية المُستدامة',
   'sdg': 'SDG Goal - هدف التنمية المستدامة',
-  'other': 'Other Development Indicator'
+  'other': 'Other Development Indicator - مؤشرات التنمية الأخرى '
 };
 
 const typeChooser = {
