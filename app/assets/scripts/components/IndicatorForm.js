7a1f0d439a2aac349b32a6b5d579a603e3f85f1d
Ahmed Nour Eldeen <ahmednoreldeen@gmail.com>
2020-09-10 11:49:14 +0200

    modify indicator data_geography labels

--- app/assets/scripts/components/IndicatorForm.js
+++ app/assets/scripts/components/IndicatorForm.js
@@ -61,7 +61,7 @@ export const schema = {
     data_geography: {
       title: 'Data Geography',
       type: 'boolean',
-      enumNames: ['Governorate - ', 'District - ']
+      enumNames: ['Governorate - a2 - محافظة', 'District - a2 - مركز']
     },
     data: {
       type: 'string',
