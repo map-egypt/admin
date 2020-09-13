1a113ce47aafc546ae457284e2ff69ddb9fd2926
Ahmed Nour Eldeen <ahmednoreldeen@gmail.com>
2020-09-13 21:33:50 +0200

    fix loan details title

--- app/assets/scripts/components/ProjectForm.js
+++ app/assets/scripts/components/ProjectForm.js
@@ -210,7 +210,7 @@ export const schema = {
             properties: {en: {type: 'string', title: 'Type of Fund'}, ar: {type: 'string'}}
           },
           loan_details: {
-            title: 'kokokwwww',// 'Loan Details( FOR LOAN TYPE ONLY) - (تفاصيل القرض (في حالة القروض فقط',
+            title: 'Loan Details( FOR LOAN TYPE ONLY) - (تفاصيل القرض (في حالة القروض فقط',
             type: 'object',
             properties: {
               interest_rate: {type: 'number', title: 'Interest Rate - سعر الفائدة'},
