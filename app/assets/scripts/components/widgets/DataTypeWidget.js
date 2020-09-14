81dcc3a68fa3a58c72a3b462a0f5217d43f3189c
Ahmed Nour Eldeen <ahmednoreldeen@gmail.com>
2020-09-14 17:02:35 +0200

    modify data types description

--- app/assets/scripts/components/widgets/DataTypeWidget.js
+++ app/assets/scripts/components/widgets/DataTypeWidget.js
@@ -30,15 +30,18 @@ export default class DataTypeWidget extends React.Component {
           <h3 className="header-small">Type of Data Examples</h3>
           <div className="field-example-item">
             <h4 className="header-smallest">Sequential - متتابع</h4>
-            <p>Data is an increasing or decreasing range</p>
+            <p>Data is an increasing or decreasing range <br/>(e.g. 0,1,2,3,4,5 ...)</p>
+            <p className="ar">البيانات التي يتزايد نطاقها أو يتناقص (مثال: 0,1,2,3,4,5 ...)</p>
           </div>
           <div className="field-example-item">
             <h4 className="header-smallest">Diverging - متشعب</h4>
-            <p>Data highlights two opposing extremes</p>
+            <p>Data highlights two opposing extremes <br/>(e.g. -1 &larr; 0 &rarr; 1)</p>
+            <p className="ar">البيانات التي تسلط الضوء على حدين متعارضين<br/> (مثال: -1 &rarr; 0 &larr; 1)</p>
           </div>
           <div className="field-example-item">
             <h4 className="header-smallest">Categorical - تصنيفي</h4>
             <p>Data represents different categories</p>
+            <p className="ar">تمثل البيانات فئات مختلفة (مثال: عالي جدا، عالي، متوسط، منخفض، منخفض جدا)</p>
           </div>
         </div>
       </div>
